539e0e3eebec0046a5a300c89ab10c249316cb96
patelas7 <patelas7@msu.edu>
2015-12-04 19:02:26 -0500

    add to game
    game update

--- Final/FinalProject/game-script.js
+++ Final/FinalProject/game-script.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
-  var html = '';
+  sessionStorage.setItem("word", "This isn't a bad test");
+  sessionStorage.setItem("hint", "This is a hint");
+  var guess = '';
+
+
   var c;
-  for (var i = 65; 90 >= i; i++) {// A-65, Z-90
+  for (var i = 65; 90 >= i; i++) {
     c = String.fromCharCode(i);
     //http://codepen.io/anon/pen/XXWajZ
     //html += '<button onclick="setLetter(\'' + c + '\');">' + c + '</button>';
-    html += '<button onclick="setLetter(\'' + c + '\');">' + c + '</button>';
+    //html += '<button onclick="setLetter(\'' + c + '\');">' + c + '</button>';
+    $('.alphabet').append('<button class=letter id="'+ c +'">' + c +'</button>');
   }
-  document.getElementById('box').innerHTML = html;
-
-  var setLetter = function(x) {
-    document.getElementById('name').innerHTML += x;
-  };
+  //document.getElementById('box').innerHTML = html;
+  $('.letter').click(function(){
+    var id = this.id;
+    alert(id);
+  });
+//  var setLetter = function(x) {
+  //  document.getElementById('name').innerHTML += x;
+//  };
 
 
 });
